5c514616f6c4739c0635d4cbc723f1697d227fc6
Rahul Singh <rs388903@gmail.com>
2023-08-04 13:45:58 +0530

    commit

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ alert("This is an alert Message");
 confirm("Do you want to Confirm?");
 confirm("Do you want to Confirm?");
 prompt("Enter the Value");
+
+
+console.log("this is" /**  i am good */ );
